075983f4e1fc28e0a6ccc52cafa525b1144e30cd
isadebugs <isa.debugs@gmail.com>
2025-11-25 20:22:05 -0600

    Corregir rutas de CSS

--- database/rufixnet.sql
+++ database/rufixnet.sql
@@ -1,7 +1,3 @@
--- Crear base de datos
-CREATE DATABASE IF NOT EXISTS rufixnet CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE rufixnet;
-
 -- Tabla de usuarios
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,7 @@ $page_title = isset($page_title) ? $page_title : 'RUFIXNET';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?></title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="/rufixnet/css/style.css">
 </head>
 <body>
     <div class="app-container">
